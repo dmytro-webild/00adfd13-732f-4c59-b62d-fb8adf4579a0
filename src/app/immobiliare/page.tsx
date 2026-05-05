2957bbd92308b470b46e2924fb5d16969e9a1abe
bender <kudin.dima@gmail.com>
2026-05-05 14:55:18 +0000

    Switch to version 3: modified src/app/immobiliare/page.tsx

--- src/app/immobiliare/page.tsx
+++ src/app/immobiliare/page.tsx
@@ -3,7 +3,7 @@
 import { ThemeProvider } from "@/providers/themeProvider/ThemeProvider";
 import ReactLenis from "lenis/react";
 import FeatureCardSeven from '@/components/sections/feature/FeatureCardSeven';
-import FooterBaseCard from '@/components/sections/footer/FooterBaseCard';
+import FooterMedia from '@/components/sections/footer/FooterMedia';
 import NavbarStyleApple from '@/components/navbar/NavbarStyleApple/NavbarStyleApple';
 import FaqDouble from '@/components/sections/faq/FaqDouble';
 
@@ -86,8 +86,9 @@ export default function LandingPage() {
   </div>
 
   <div id="footer" data-section="footer">
-      <FooterBaseCard
+      <FooterMedia
       logoText="SBR Consulting"
+      imageSrc="https://webuild-dev.s3.eu-north-1.amazonaws.com/users/user_3D4KC0FsaA7ldxeqCrwzWFE7quv/modern-high-end-architectural-detail-cle-1777989649870-d9a73473.png?_wi=1"
       columns={[
         {
           title: "Servizi",          items: [
@@ -106,6 +107,7 @@ export default function LandingPage() {
           ],
         },
       ]}
+      copyrightText="© 2024 SBR Consulting. Tutti i diritti riservati."
     />
   </div>
       </ReactLenis>
